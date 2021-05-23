33e9c37588cee5d055e8062e439df0557cb06868
zonder888 <gazflair3d@gmail.com>
2021-05-23 20:34:43 +0000

    init

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests
+from pprint import pprint
 from config import open_weather_token
 
 def get_weather(city, open_weather_token):
     try:
         r = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}"
+            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
         )
         data = r.json()
-        print(data)
+        pprint(data)
 
     except Exception as ex:
         print(ex)
