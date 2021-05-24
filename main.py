8805a3911c8de437f34becaa60b9a659cbedbe07
zonder888 <gazflair3d@gmail.com>
2021-05-24 19:48:37 +0000

    init

--- main.py
+++ main.py
@@ -46,27 +46,33 @@ def get_weather(city, open_weather_token):
         sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
         length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
             data["sys"]["sunrise"])
+
         zone1 = str(time_zone)
         zone2 = zone1.replace("-", " ")
         zone3 = zone2.replace(":", " ")
         zone4 = zone3.replace(" ", " ")
         zone5 = [zone4[0:12]]
         zone5.append(zone4[11:13])
-        zone5.append(zone4[14:19])
+        zone5.append(zone4[14:16])
+        zone5.append(zone4[17:19])
+        dict_time = {}
+        dict_time["hour"] = int(zone5[1])
+        dict_time["minutes"] = int(zone5[2])
 
 
-        print(zone5)
-        # Current time in UTC
-        now_utc = datetime.datetime.now(datetime.timezone.utc)
-        tm_rus = timezone('Europe/Moscow')
-        time_RU = now_utc.astimezone(tm_rus)
-        print("UTC Time: ", time_RU)
-        #print(f"***{datetime.datetime.now().strftime(f'%Y-%m-%d %H:%M')}***\n"
-        print(f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+        print(dict_time)
+        # # Current time in UTC Правильное время
+        # now_utc = datetime.datetime.now(datetime.timezone.utc)
+        # tm_rus = timezone('Europe/Moscow')
+        # time_RU = now_utc.astimezone(tm_rus)
+        # print("UTC Time: ", time_RU)
+        print(f"Дата: {datetime.datetime.now().strftime(f'%Y-%m-%d')}\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
               #f"Зона : {time_zone}\n"
               f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
               f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
-              f"Хорошего дня!"
+              f"Хорошего дня!\n"
+              f"МСК: {datetime.datetime.now().strftime(f'%H:%M')}"
               )
 
     except Exception as ex:
